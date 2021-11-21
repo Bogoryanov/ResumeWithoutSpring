ef5a9c53383cce45730b206bebb917cf42a3d5f6
Bogoryanov <psymode@211.ru>
2021-11-21 23:06:38 +0700

    Add files via upload

--- src/main/java/hero_profile/hero_profile.java
+++ src/main/java/hero_profile/hero_profile.java
@@ -1,2 +1,55 @@
-package hero_profile;public class hero_profile {
+package hero_profile;
+
+import java.util.ArrayList;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "hero_name")
+public class hero_profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "hero_name")
+    private String name;
+
+
+    private int age;
+
+    public hero_profile() {
+    }
+
+    public hero_profile(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+      public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class main {
+
+import hero_profile.hero_profile;
+import services.UserService;
+
+import java.sql.SQLException;
+
+class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserService();
+        hero_profile user = new hero_profile("Alex",26);
+        userService.save(user);
+    }
 }
